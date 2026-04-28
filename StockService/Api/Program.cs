2c8cf635e55dadfb2f12608c1c84b922a33f9ec2
Alzatiyo <alzatiyo10@gmail.com>
2026-04-27 19:29:50 -0500

    fix: cambios en Program.cs

--- StockService/Api/Program.cs
+++ StockService/Api/Program.cs
@@ -6,20 +6,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Registra todos los servicios de Infrastructure, Application y Domain
 builder.Services.AddInfrastructureServices(builder.Configuration);
 
-// Controllers y Swagger (los controllers están en Infrastructure)
+// Controllers y Swagger (los controllers estï¿½n en Infrastructure)
 builder.Services.AddControllers()
     .AddApplicationPart(typeof(Infrastructure.Adapters.Rest.ProductController).Assembly);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.Migrate();
-}
 
 // Habilitar CORS
 builder.Services.AddCors(options =>
@@ -32,8 +24,22 @@ builder.Services.AddCors(options =>
     });
 });
 
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
+
+
 app.UseSwagger();
 app.UseSwaggerUI();
+
+// Usar CORS antes de Authorization
+app.UseCors("AllowAll");
+
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
