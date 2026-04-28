677fd59598345ebbdafbe98f24ba9e35282ddb6c
Krampuuz <hyehicol@gmail.com>
2026-04-27 19:17:19 -0500

    Se habilita el CORS para poder usar los servicios desde el front

--- AuditService/Api/Program.cs
+++ AuditService/Api/Program.cs
@@ -5,12 +5,23 @@ var builder = WebApplication.CreateBuilder(args);
 // Registra todos los servicios de Infrastructure, Application y Domain
 builder.Services.AddInfrastructureServices(builder.Configuration);
 
-// Controllers y Swagger (los controllers están en Infrastructure)
+// Controllers y Swagger (los controllers estï¿½n en Infrastructure)
 builder.Services.AddControllers()
     .AddApplicationPart(typeof(Infrastructure.Adapters.Rest.AuditController).Assembly);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Habilitar CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
